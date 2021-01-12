18cbee00db854159f85e51db8a2bbc00e9661e03
Christian Kriech <christian@krie.ch>
2021-01-12 12:28:57 -0800

    started implementing dependency injection

--- cooltool.fish
+++ cooltool.fish
@@ -7,6 +7,24 @@ end
     # echo "First, type `ct` in your terminal, followed by any arguments, then enter the name of the command you want to execute and press `Enter` or `Return`. See --help for more details"
 # end
 
+## rough order of TODOs:
+# 1. Add extremely basic dep management
+#   - add default resolvers and download libs
+#   - download dep's deps?
+# 2. Set up testing
+#   - download junit
+#   - run tests with test command
+# 3. Create jars with deps
+#   - dump the 'mono-folder' joke if becomes too much of a pain
+# 4. Add support for modules
+#   - this will need a lot of research
+# 5. Write a slightly more extensive application using recent java features
+# 6. Make something useful and use tool to push up to resolver
+
+# Less important bits or just that might come up
+# - fix asdf installation
+# - generalize reading from project.cool
+# - save data in a .cool folder instead of all env vars and write functionings to read and set those
 function ct
     set_color -b black
     set_color brmagenta
@@ -69,6 +87,8 @@ end
 function _ct_reload
     _ct_read_cool_file
     _ct_tag_files
+    _ct_init
+    _ct_install_java
     set -xg _ct_proj_directory $PWD #make sure this doesn't change
 end
 
@@ -97,6 +117,10 @@ function _ct_read_cool_file
     else
         set file $argv[1]
     end
+    set -e _ct_proj_name
+    set -e _ct_mainclass
+    set -e _ct_java_ver
+    set -xg _ct_use_local_asdf "false"
     while read -la -d "\n" line
         switch $line
             case "project=*"
@@ -105,9 +129,52 @@ function _ct_read_cool_file
                 set -xg _ct_mainclass (_snip $line)
             case "java_version=*"
                 set -xg _ct_java_ver  (_snip $line)
+            case "use_local_asdf=*"
+                if test (_snip $line) = "true"
+                    set -xg _ct_use_local_asdf "true" 
+                end
             case "*"
         end
     end < $file
+end
+
+#if no _ct_java_ver assigned, use openjdk-15
+#check if _ct_java_ver installed, skip install if true
+#check if _ct_java_ver is a real asdf version, return false if true
+#install version if needed
+function _ct_install_java
+    set -x _ct_default_java "openjdk-15" #only support exact strings that asdf expects
+
+    if test -z $_ct_java_ver
+        echo "No java version set, using $_ct_default_java instead"
+        set _ct_java_ver $_ct_default_java
+    end
+
+
+    if contains $_ct_java_ver ($_ct_asdf list java | string split ' ')
+        echo "Correct java version already installed"
+    else
+        if contains $_ct_java_ver ($_ct_asdf list all java | string split ' ')
+            echo installing $_ct_java_ver
+            $_ct_asdf install java $_ct_java_ver
+        else
+            echo "$_ct_java_ver not recognized"
+            false
+            return #see if this actually returns
+        end
+    end
+end 
+
+function _ct_assign_java 
+    # if test (_ct_asdf_check_current_version $_ct_java_ver) = "true" #does that actually work?
+        # echo "Correct java version already assigned"
+    # else
+    echo assigning $_ct_java_ver to local
+    $_ct_asdf local java $_ct_java_ver  
+end
+
+function _ct_asdf_check_current_version
+    echo "false"
 end
 
 function _ct_tag_files #set scope for all files in dir
@@ -130,23 +197,30 @@ function _snip
     echo $argv[1] | sed 's/.*=//g' 
 end
 
+#TODO get asdf to only install for project, not for shell
 function _ct_init
-    #download asdf for java version management
-    #check if init is finished already or needs repairing?
-    if test ! \( -e .asdf \) -o ! \( -d .asdf \) #if not .asdf exsits and is a dir
-        curl -o asdf.zip https://codeload.github.com/asdf-vm/asdf/zip/v0.8.0
-        unzip asdf.zip
-        mkdir .asdf
-        cp -r asdf-0.8.0/lib .asdf/lib
-        cp -r asdf-0.8.0/bin .asdf/bin
-        rm -r asdf-0.8.0
-        rm asdf.zip
-        set -l ASDF_DATA_DIR .asdf
-        .asdf/bin/asdf plugin-add java https://github.com/halcyon/asdf-java.git
+
+    if test $_ct_use_local_asdf = "true"
+        set -xg _ct_asdf "asdf"
     else
-        echo "??????????????"
+        echo "downloading asdf is not supported yet!"
+        #download asdf for java version management
+        #check if init is finished already or needs repairing?
+        # if test ! \( -e .asdf \) -o ! \( -d .asdf \) #if not .asdf exsits and is a dir
+            # curl -o asdf.zip https://codeload.github.com/asdf-vm/asdf/zip/v0.8.0
+            # unzip asdf.zip
+            # mkdir .asdf
+            # cp -r asdf-0.8.0/lib .asdf/lib
+            # cp -r asdf-0.8.0/bin .asdf/bin
+            # rm -r asdf-0.8.0
+            # rm asdf.zip
+            # set -l ASDF_DATA_DIR .asdf
+            # .asdf/bin/asdf plugin-add java https://github.com/halcyon/asdf-java.git
+        # else
+            # echo "??????????????"
+        # end
+        #.asdf/asdf.sh plugin add https://github.com/halcyon/asdf-java.git
     end
-    #.asdf/asdf.sh plugin add https://github.com/halcyon/asdf-java.git
 end
 
 function _ct_compile
@@ -163,7 +237,6 @@ function _ct_compile
 end
  
 function _ct_run 
-    #download java?
     #assumes we're in the correct dir
     java $_ct_mainclass
 end
@@ -197,4 +270,44 @@ function _ct_get_scope_file
                 echo "main_resource"
         end
     end
+end
+
+#only maven for now
+function _ct_download_dep
+    set -l _repo https://repo.maven.apache.org/maven2
+
+    set -l _dep (string split ":" $argv[1])
+
+    if test ! (count $_dep) -eq 3
+        echo wtf
+    end
+
+    #"package":"lib":"version"
+
+    set -l _package (string replace "." "/" $_dep[1])  # replace dots with slashes 
+    set -l _name $_dep[2]
+    set -l _version $_dep[3]
+    set -l _jar "$_name-$_version.jar"
+
+#download not as big as expected?
+    echo "$_repo/$_package/$_name/$_version/$_jar"
+    curl -o $_jar "$_repo/$_package/$_name/$_version/$_jar"
+end
+
+#.cool file
+#format:
+# like project.cool but used to maintain state instead of env vars
+# how do you prevent overwrites from multiple processes?
+#
+# 
+# random but:
+# fish .set notes:
+# - default if no scope is for the *current function*
+# -     this is different than -l
+function _ct_read
+
+end
+
+function _ct_write
+
 end
